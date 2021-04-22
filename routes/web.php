895292dddca858351abb126a08f31252c6c42b17
Mariam de Jesus Delgado Zerquera <mariam.delgado4258@alumnos.udg.mx>
2021-04-21 21:36:51 -0500

    Migracion para tabla tareas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('tarea', [TareaController::class, 'index']);
